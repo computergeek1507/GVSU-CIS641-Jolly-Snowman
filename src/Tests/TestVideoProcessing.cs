5f33f8b5e6a939b019a2b59dd90cb5fc41b77d67
George Ebeling <ebelingg@mail.gvsu.edu>
2021-11-01 18:11:25 -0400

    Added some tests with short videos to test processing

--- src/Tests/TestVideoProcessing.cs
+++ src/Tests/TestVideoProcessing.cs
@@ -6,13 +6,13 @@ using System.Threading.Tasks;
 using NUnit.Framework;
 using Emgu.CV;
 
+
 namespace Emgu_Test.Tests
 {
     [TestFixture]
     public class TestVideoProcessing
     {
         private Mat testMat;
-        private LightManager testMan;
 
         [SetUp]
         public void SetUp()
@@ -28,27 +28,47 @@ namespace Emgu_Test.Tests
         }
 
         [Test]
-        public void something()
+        public void VerifyFrame()
         {
             Assert.IsNotNull(testMat);
         }
 
-
         [Test]
         public void TestDrawnFoundNodes()
         {
-            testMan = new LightManager();
-
+            LightManager testMan = new LightManager();
             VideoSettings testSet = new VideoSettings();
+
             VideoProcessing vid = new VideoProcessing(testMan);
             vid.LoadVideo(testSet);
 
             vid.ProcessFrame(testMat);
             Assert.Greater(testMan.GetLights().Count(), 0, "Lights should have been detected");
-
-            
         }
 
+        [Test]
+        public void TestVideo_BeginningAndOne()
+        {
+            LightManager testMan = VideoHelper.ProcessVideo( @"../../artifacts/TestVideos/Test_BeginningAndOne.mp4");
+            
+            Assert.Greater(testMan.GetLights().Count(), 0, "Lights should have been detected");
+        }
+
+        [Test]
+        public void TestVideo_BeginningAndNone()
+        {
+            LightManager testMan = VideoHelper.ProcessVideo(@"../../artifacts/TestVideos/Test_BeginningAndNone.mp4");
+
+            Assert.AreEqual(testMan.GetLights().Count(), 0, "No lights should have been detected");
+        }
+
+        [Test]
+        public void TestVideo_NoBeginningAndOne() // #TODO What is the correct outcome for this test?
+        {
+            LightManager testMan = VideoHelper.ProcessVideo(@"../../artifacts/TestVideos/Test_NoBeginningAndOne.mp4");
+
+            Assert.AreEqual(testMan.GetLights().Count(), 0, "One light should have been detected, maybe?");
+        }
         
     }
 }
---
+++ src/Tests/VideoHelper.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Emgu_Test.Tests
+{
+    class VideoHelper
+    {
+        public static LightManager ProcessVideo(String filename)
+        {
+            var testMan = new LightManager();
+
+            VideoSettings vidSet = LoadSettings();
+            vidSet.FileName = filename;
+            VideoProcessing vidProc = new VideoProcessing(testMan);
+            vidProc.LoadVideo(vidSet);
+
+            vidProc.ProcessVideo();
+
+            return testMan;
+        }
+
+        /**
+         * Stolen from MainForm.cs
+         */ 
+        private static readonly string _appDataFolder = string.Format("{0}{1}{2}", Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), Path.DirectorySeparatorChar, "Emgu_Test");
+
+        public static VideoSettings LoadSettings()
+        {
+            var settingFile = _appDataFolder + Path.DirectorySeparatorChar + "settings.xml";
+
+            try
+            {
+                if (File.Exists(settingFile))//see if file exists
+                {
+                    XmlSerializer serializer = new XmlSerializer(typeof(VideoSettings));
+                    FileStream reader = new FileStream(settingFile, FileMode.Open);
+                    var _videoSettings = (VideoSettings)serializer.Deserialize(reader);
+                    reader.Close();
+                    return _videoSettings;
+                }
+                return null;
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+    }
+}
