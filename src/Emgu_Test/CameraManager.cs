95f22e3fd4a26c1933cbb26fe8b0f7d1c68e36db
Scott Hanson <scooter_seh@yahoo.com>
2021-11-15 18:05:25 -0500

    started camera manager

---
+++ src/Emgu_Test/CameraManager.cs
@@ -0,0 +1,25 @@
+﻿using DirectShowLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Emgu_Test
+{
+    class CameraManager
+    {
+        public static List<string> ListOfAttachedCameras()
+        {
+            List<string> cameras = new List<string>();
+            DirectShowLib.DsDevice[] allCameras = DirectShowLib.DsDevice.GetDevicesOfCat(FilterCategory.VideoInputDevice);
+            foreach (var cam in allCameras)
+            {
+                cameras.Add(cam.Name);
+            }
+            return cameras;
+
+        }
+    }
+}
